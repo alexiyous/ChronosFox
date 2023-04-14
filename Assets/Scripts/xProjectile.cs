346ecb19652ec5c4d67c324070a8ceff8c019d63
QaorVa <100664948+QaorVa@users.noreply.github.com>
2023-04-14 23:21:39 +0700

    projectile bounce
    - projectile bounce
    - background
    - button ui

--- Assets/Scripts/xProjectile.cs
+++ Assets/Scripts/xProjectile.cs
@@ -42,7 +42,7 @@ public class xProjectile : MonoBehaviour
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if (collision.CompareTag("Player"))
+        if (!collision.CompareTag("Enemy"))
         {
             Destroy(gameObject);
             Instantiate(impactEffect, transform.position, Quaternion.identity);
