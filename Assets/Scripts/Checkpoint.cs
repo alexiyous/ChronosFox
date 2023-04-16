b1629eccef6ea7abc29d3e5fb58634462f16eb5e
alexiyous <alexiusrenanta@gmail.com>
2023-04-16 09:27:40 +0700

    Set Continue player location

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -9,6 +9,9 @@ public class Checkpoint : MonoBehaviour
         if (other.tag == "Player")
         {
             RespawnController.instance.SetSpawn(transform.position);
+            PlayerPrefs.SetFloat("PosX", transform.position.x);
+            PlayerPrefs.SetFloat("PosY", transform.position.y);
+            PlayerPrefs.SetFloat("PosZ", transform.position.z);
         }
     }
 }
