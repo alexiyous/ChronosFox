96446558d9b4c10bcb9f9829ad1f80b2eaaa7f15
alexiyous <alexiusrenanta@gmail.com>
2023-04-16 21:35:09 +0700

    Main Level Selesai
    minus bear

--- Assets/Scripts/Checkpoint.cs
+++ Assets/Scripts/Checkpoint.cs
@@ -4,6 +4,13 @@ using UnityEngine;
 
 public class Checkpoint : MonoBehaviour
 {
+    private Animator anim;
+
+    private void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.tag == "Player")
